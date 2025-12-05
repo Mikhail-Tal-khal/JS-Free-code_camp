015f3317cfb9f8dd73d60cf576142aa4f21761d2
Mikhail-Tal-khal <khaledkhlyan@gmail.com>
2025-12-05 21:58:26 +0100

    React learning app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
-import {Welcome} from './Welcome.jsx'
-import {Button} from './Button.jsx'
-import {Hello} from './Hello.jsx'
+import { Welcome } from './Welcome.jsx'
+import { Button } from './Button.jsx'
+import { Hello } from './Hello.jsx'
+import { Product } from './Product.jsx'
 import './App.css'
 
 function App() {
@@ -9,7 +10,10 @@ function App() {
     <div>
 
       <Hello />
-      <Welcome />
+      <Welcome name='kalvani' alias="Ranger" />
+      <Welcome name='diana' alias="Guard" />
+      <Welcome name='neymer' alias="Defender" />
+      <Product title="GaminingLaptop" Price={1299.56} inStock={true} Categories={("Electronics", "Computers", "Gaming")} />
       <Button />
     </div>
   )
